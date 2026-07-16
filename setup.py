487d91e791c99041d93b757f5dd1d726378ea86f
H-Hasnat <hasnathasin.67@gmail.com>
2026-07-16 14:35:07 +0600

    data ingestion,validation and transformation added

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Vikash Das",
-    author_email="vikashdas770@gmail.com",
+    author="Hasin Hasnat",
+    author_email="hasnathasin.67@gmail.com",
     packages=find_packages()
 )
+
+
+
+
+
